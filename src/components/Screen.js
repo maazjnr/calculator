f49100ae623cc196c90b84e4d32af8b7606e27d2
Maaz Junior <goalmaaz@gmail.com>
2022-07-25 00:11:00 +0100

    i designed all the css m=styling for my calculator app

---
+++ src/components/Screen.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { CalcContext } from "../Context/CalContext";
+import { ReactTextField } from "react-textfield";
+
+
+const Wrapper = () => {
+
+    const {calc} = useContext(CalcContext)
+
+    return(
+        <div className="screen">
+            <h1 className="resultNumber">
+                {calc.num ? calc.num : calc.res}
+            </h1>
+        </div>
+    )
+}
+
+export default Wrapper
